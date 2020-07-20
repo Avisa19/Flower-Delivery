600fe603ab35af8b84c67e41e0ebc60e1fdeab26
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-20 13:21:31 -0700

    Add section one to CollectionViewController

--- Flower-Delivery/Flower-Delivery/Controllers/FlowerController.swift
+++ Flower-Delivery/Flower-Delivery/Controllers/FlowerController.swift
@@ -10,9 +10,28 @@ import SwiftUI
 
 class FlowerController: UICollectionViewController {
     
+    fileprivate let CELL_ID = "CellID"
+    
     init() {
-        let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewCompositionalLayout { (sectionNumber, _) -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem.init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            item.contentInsets.trailing = 2
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .paging
+            return section
+        }
         super.init(collectionViewLayout: layout)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CELL_ID, for: indexPath)
+        cell.backgroundColor = .systemBlue
+        return cell
     }
     
     required init?(coder: NSCoder) {
@@ -23,6 +42,7 @@ class FlowerController: UICollectionViewController {
         super.viewDidLoad()
         
         collectionView.backgroundColor = UIColor.init(white: 0.5, alpha: 0.1)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: CELL_ID)
     }
 }
 
